67cf3b930d0298d034a1d596a3e8a6d0fbd17ea2
Parth <parth1493@gmail.com>
2017-05-07 16:34:43 -0400

    Harjinder3

--- app/src/main/java/com/example/a1691547/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a1691547/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //h123 gyugyhjhj dfg
+        //h123 gyugyhjhj
     }
 }
